3fc3c0f58d6b9e7ff9eec6402f70c0f5c6a8c458
jomon1234 <35091984+jomon1234@users.noreply.github.com>
2024-01-31 03:44:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.get('/', (req, res) => res.send('Hello World From NodeJS!'))
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Docker World!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on port ${port}`);
+});
