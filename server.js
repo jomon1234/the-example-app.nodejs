1a064ec0bb73f7dc60da7800a2dda055734d349a
jomon1234 <35091984+jomon1234@users.noreply.github.com>
2024-01-31 01:34:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World From NodeJS!'));
-
-// Update the following line to bind to all network interfaces
-app.listen(port, '0.0.0.0', () => console.log(`Example app listening at http://host:${port}`));
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (req, res) => res.send('Hello World From NodeJS!'))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
